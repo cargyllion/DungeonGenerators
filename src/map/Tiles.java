b9599532c3df82d0ae09ef803ec92148290a55c0
cargyllion <141673290+cargyllion@users.noreply.github.com>
2024-01-23 13:57:07 -0800

    Update Tiles.java

--- src/map/Tiles.java
+++ src/map/Tiles.java
@@ -1,22 +1,22 @@
 package map;
 
-public class Tiles {
+public class tiles {
 
 	public int tileID;
 	public boolean isSolid;
 	
-	public Tiles[] tileset = new Tiles[256];
+	public tiles[] tileset = new tiles[256];
 	
-	public static Tiles stoneWall = new Tiles(0,true);
-	public static Tiles stoneFloor = new Tiles(1,false);
-	public static Tiles roomWall = new Tiles(2,true);
-	public static Tiles roomFloor=new Tiles(3,false);
-	public static Tiles stairsDown = new Tiles(4,false);
-	public static Tiles stairsUp = new Tiles(5,false);
-	public static Tiles water = new Tiles(6,false);
-	public static Tiles door = new Tiles(7,true);
+	public static tiles stoneWall = new tiles(0,true);
+	public static tiles stoneFloor = new tiles(1,false);
+	public static tiles roomWall = new tiles(2,true);
+	public static tiles roomFloor=new tiles(3,false);
+	public static tiles stairsDown = new tiles(4,false);
+	public static tiles stairsUp = new tiles(5,false);
+	public static tiles water = new tiles(6,false);
+	public static tiles door = new tiles(7,true);
 	
-	public Tiles(int id, boolean solid) {
+	public tiles(int id, boolean solid) {
 		this.isSolid=solid;
 		this.tileID=id;
 		if(tileset[id]==null) {
