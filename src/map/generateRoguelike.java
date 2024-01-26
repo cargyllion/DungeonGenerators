93b07273151c47e0bb66f0f7111e96a213e9b3ff
cargyllion <141673290+cargyllion@users.noreply.github.com>
2024-01-26 14:39:07 -0800

    condensed int arrays into Vectors.

--- src/map/generateRoguelike.java
+++ src/map/generateRoguelike.java
@@ -17,40 +17,40 @@ public class generateRoguelike extends generator{
   /**
   * Pick where we'll start digging our corridor from, as well as what direction the door is facing.
   */
-	public int[][] chooseRandomExit(Room room){
-		int[][] exits = new int[2][2];
+	public Vector2[] chooseRandomExit(Room room){
+		Vector2[] posangle = {new Vector2(0,0),new Vector2(0,0)};
 		
 		switch(rand.rand_choice(4)) {
 			case 0:
-				exits[0][0]=room.minx;
-				exits[0][1]=rand.rand_range(room.miny+1, room.maxy-1);
-				exits[1][0]=-1;
-				exits[1][1]=0;
+				posangle[0].x=room.minx;
+				posangle[0].y=rand.rand_range(room.miny+1, room.maxy-1);
+				posangle[1].x=-1;
+				posangle[1].y=0;
 				break;
 			case 1:
-				exits[0][0]=room.maxx;
-				exits[0][1]=rand.rand_range(room.miny+1, room.maxy-1);
-				exits[1][0]=1;
-				exits[1][1]=0;
+				posangle[0].x=room.maxx;
+				posangle[0].y=rand.rand_range(room.miny+1, room.maxy-1);
+				posangle[1].x=-1;
+				posangle[1].y=0;
 				break;
 			case 2:
-				exits[0][0]=rand.rand_range(room.minx+1, room.maxx-1);
-				exits[0][1]=room.miny;
-				exits[1][0]=0;
-				exits[1][1]=-1;
+				posangle[0].x=rand.rand_range(room.minx+1, room.maxx-1);
+				posangle[0].y=room.miny;
+				posangle[1].x=0;
+				posangle[1].y=-1;
 				break;
 			case 3:
-				exits[0][0]=rand.rand_range(room.minx+1, room.maxx-1);
-				exits[0][1]=room.maxy;
-				exits[1][0]=0;
-				exits[1][1]=1;
+				posangle[0].x=rand.rand_range(room.minx+1, room.maxx-1);
+				posangle[0].y=room.maxy;
+				posangle[1].x=0;
+				posangle[1].y=1;
 				break;
 		}
-		return blarg;
+		return posangle;
 	}
 
   /**
-  * Create a randomly-sized room. Rooms will generate with interior dimensions ranging from 2x2 tiles to 7x7.
+  * Create a randomly-sized room. Rooms will generate with interior dimensions ranging from 2x2 Tiles to 7x7.
   */
 	public Room buildRandomRoom(Room room) {
 		room.minx=rand.rand_range(1, floor.width-4);
@@ -65,15 +65,15 @@ public class generateRoguelike extends generator{
   */
 	private boolean checkRoomFits(Room room) {
 		int x,y;
-    //Check the room boundaries. We don't want to override any existing level data.
+    		//Check the room boundaries. We don't want to override any existing level data.
 		for(y=room.miny;y<room.maxy;y++) {
 			for(x=room.minx;x<room.maxx;x++) {
-				if(floor.getTile(x, y)!=tiles.stoneWall.tileID) {
+				if(floor.getTile(x, y)!=Tiles.stoneWall.tileID) {
 					return false;
 				}
 			}
 		}
-    //Check the room's immediate surroundings. We don't want to generate along the map edge or directly adjacent to an existing room. This looks ugly and causes problems with drawing the corridors.
+   		 //Check the room's immediate surroundings. We don't want to generate along the map edge or directly adjacent to an existing room. This looks ugly and causes problems with drawing the corridors.
 		for(x=room.minx-1;x<room.maxx+1;x++) {
 			if(floor.isMapWall(x,room.miny-1)) {
 				return false;
@@ -94,47 +94,46 @@ public class generateRoguelike extends generator{
 	}
 
 	/**
-  *Pretty straight forward: Place the room object onto the map by drawing a rectangle with the room's bounding box.
-  */
+  	*Pretty straight forward: Place the room object onto the map by drawing a rectangle with the room's bounding box.
+ 	*/
 	public void drawRoom(Room room) {
-			for(int x=room.minx;x<=room.maxx;x++) {
-				for(int y=room.miny;y<=room.maxy;y++) {
-					if(x==room.minx||x==room.maxx||y==room.miny||y==room.maxy) {
-						floor.setTile(x, y, tiles.roomWall.tileID);
-						
-					}else {
-						floor.setTile(x, y, tiles.roomFloor.tileID);
-					}
+		for(int x=room.minx;x<=room.maxx;x++) {
+			for(int y=room.miny;y<=room.maxy;y++) {
+				if(x==room.minx||x==room.maxx||y==room.miny||y==room.maxy) {
+					floor.setTile(x, y, Tiles.roomWall.tileID);		
+				}else {
+					floor.setTile(x, y, Tiles.roomFloor.tileID);
 				}
 			}
+		}
 	}
 
   /**
-  * We don't want to indiscriminately place floor tiles, so this function is used to place different path tiles based on what's currently occupying the space.
+  * We don't want to indiscriminately place floor Tiles, so this function is used to place different path Tiles based on what's currently occupying the space.
   */
 	private void setPathTile(int x, int y) {
 		int newtile, oldtile;
 		oldtile = floor.getTile(x, y);
 		newtile=oldtile;
-		if(oldtile==tiles.stoneFloor.tileID) {
-      //There's no reason to continue here.
+		if(oldtile==Tiles.stoneFloor.tileID) {
+     		//There's no reason to continue here.
 			return;
-		}else if(oldtile==tiles.stoneWall.tileID) {
-      newtile = tiles.stoneFloor.tileID;
-		} else if(oldtile==tiles.door.tileID||oldtile==tiles.roomWall.tileID) {
-			newtile=tiles.door.tileID;
+		}else if(oldtile==Tiles.stoneWall.tileID) {
+      			newtile = Tiles.stoneFloor.tileID;
+		} else if(oldtile==Tiles.door.tileID||oldtile==Tiles.roomWall.tileID) {
+			newtile=Tiles.door.tileID;
 		}
 		floor.setTile(x, y, newtile);
 		
 	}
 	
 	/**
-  *Draw a corridor between the two XY pairs.
-  */
+  	*Draw a corridor between the two XY pairs.
+  	*/
 	boolean findPath(int[] s, int[] e) {
 		int[] d = new int[2];
 		int i,dir,wallhit;
-		int[] s=new int[2],e=new int[2],n=new int[2];
+		int[] n=new int[2];
 		int len=0;
 		int ncnt=0;
 
@@ -202,14 +201,14 @@ public class generateRoguelike extends generator{
 		return true;
 	}
 
-	
-	boolean drawCorridor(int[][] s, int[][] e) {
+	boolean drawCorridor(Vector2[] s, Vector2[] e) {
 		boolean success;
-		success=findPath(s,e);
+		success=findPath(new Vector2(s).toArr(),new Vector2(e).toArr());
+		// Draw our doors.  We only do this if our path creation was successful.
 		if(success) {
-      // Draw our doors.  We only do this if our path creation was successful.
-			setPathTile(s[0][0],s[0][1]);
-			setPathTile(e[0][0],e[0][1]);
+			System.out.println("Doors");
+			setPathTile(s[0].x,s[0].y);
+			setPathTile(e[0].x,e[0].y);
 		}
 		return success;
 	}
@@ -245,12 +244,9 @@ public class generateRoguelike extends generator{
     // If we fail to connect 40 times, we declare this dungeon sucky and go again...
 		fails=0;
 		for(i=0;i<rooms.size()-1;i++) {
-			int[][] vecs = new int[2][2];
-			int[][] vecs2= new int[2][2];
 			src=rand.rand_range(0,i);
-			
-			vecs = chooseRandomExit(rooms.get(src));
-			vecs2 = chooseRandomExit(rooms.get(i+1));
+			Vector2[] vecs = chooseRandomExit(rooms.get(src));
+			Vector2[] vecs2 = chooseRandomExit(rooms.get(i+1));
 
       //Draw a corridor between these two rooms.
 			if(!drawCorridor(vecs,vecs2)) {
@@ -275,8 +271,8 @@ public class generateRoguelike extends generator{
 				Room rm = rooms.get(room);
 				x=rand.rand_range(rm.minx+1, rm.maxx-1);
 				y=rand.rand_range(rm.miny+1, rm.maxy-1);
-				if(floor.getTile(x, y)==tiles.roomFloor.tileID) {
-					floor.setTile(x,y,tiles.stairsDown.tileID);
+				if(floor.getTile(x, y)==Tiles.roomFloor.tileID) {
+					floor.setTile(x,y,Tiles.stairsDown.tileID);
 					break;
 				}
 			}
@@ -285,8 +281,8 @@ public class generateRoguelike extends generator{
 				Room rm = rooms.get(room);
 				x=rand.rand_range(rm.minx+1, rm.maxx-1);
 				y=rand.rand_range(rm.miny+1, rm.maxy-1);
-				if(floor.getTile(x, y)==tiles.roomFloor.tileID) {
-					floor.setTile(x,y,tiles.stairsUp.tileID);
+				if(floor.getTile(x, y)==Tiles.roomFloor.tileID) {
+					floor.setTile(x,y,Tiles.stairsUp.tileID);
 					break;
 				}
 			}
